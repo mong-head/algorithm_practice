8d34552a8fb8464002a14566bf0ae6470db387a0
mong_head <camara1melon315@gmail.com>
2020-10-04 10:52:04 +0900

    test

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-  int st[4];
-  fill_n(st,4,1);
-  for(int i : st) cout << i <<' ';
+  vector<string> c = {"leo"};
+  cout << c[1];
 
   return 0;
 }
